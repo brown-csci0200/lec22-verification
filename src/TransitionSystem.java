f5665333a873a63f0713e162d0e6de4dcbbd480c
Elijah Rivera <elijah_rivera@brown.edu>
2024-03-16 08:38:22 -0400

    Code from end of lecture

--- src/TransitionSystem.java
+++ src/TransitionSystem.java
@@ -1,41 +1,54 @@
 package src;
 
+import javax.swing.plaf.nimbus.State;
 import java.util.*;
 import java.util.function.Function;
 
-public class TransitionSystem {
-    TrafficState startState;
-    HashMap<TrafficState, Vertex> allVertices;
+public class TransitionSystem <StateType> {
+    StateType startState;
+    HashMap<StateType, Vertex<StateType>> allVertices;
 
-    public TransitionSystem(TrafficState start) {
+    public TransitionSystem(StateType start) {
         this.allVertices = new HashMap<>();
         this.addVertex(start);
         this.startState = start;
     }
 
-    public void addVertex(TrafficState label) {
+    public void addVertex(StateType label) {
         if (!allVertices.containsKey(label)) {
-            this.allVertices.put(label, new Vertex(label));
+            this.allVertices.put(label, new Vertex<StateType>(label));
         }
     }
 
-    public void addTransition(String transitionLabel, TrafficState fromLabel, TrafficState toLabel) {
+    public void addTransition(String transitionLabel, StateType fromLabel, StateType toLabel) {
         this.addVertex(fromLabel);
         this.addVertex(toLabel);
-        Vertex startV = this.allVertices.get(fromLabel);
-        Vertex endV = this.allVertices.get(toLabel);
+        Vertex<StateType> startV = this.allVertices.get(fromLabel);
+        Vertex<StateType> endV = this.allVertices.get(toLabel);
         startV.nextStates.put(transitionLabel, endV);
     }
 
-    // public ? runSystem
+    // Given a list of transitions the system should take (in order),
+    // produce the *trace* of the system
+    // (i.e., the list of states that the system visits, in order)
+    public List<StateType> runSystem(ArrayList<String> transitions ) {
+        Vertex<StateType> currentState = this.allVertices.get(this.startState);
+        ArrayList<StateType> result = new ArrayList<>();
+        result.add(this.startState);
+        for (String transition : transitions) {
+            currentState = currentState.getNextState(transition);
+            result.add(currentState.label);
+        }
+        return result;
+    }
 
-    public boolean canBeBad(Function<TrafficState, Boolean> badProperty) {
-        Vertex start = this.allVertices.get(this.startState);
+    public boolean canBeBad(Function<StateType, Boolean> badProperty) {
+        Vertex<StateType> start = this.allVertices.get(this.startState);
         return start.canVisitBad(badProperty);
     }
 
     public boolean canLoop() {
-        Vertex start = this.allVertices.get(this.startState);
+        Vertex<StateType> start = this.allVertices.get(this.startState);
         return start.canLoop();
     }
 }
--- src/Vertex.java
+++ src/Vertex.java
@@ -1,30 +1,33 @@
 package src;
 
+import javax.swing.plaf.nimbus.State;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.function.Function;
 
-public class Vertex {
-    TrafficState label;
+public class Vertex<StateType> {
+    StateType label;
     // map from transition labels to the vertex that transition goes to
-    HashMap<String, Vertex> nextStates;
+    HashMap<String, Vertex<StateType>> nextStates;
+    // key - String that represents a transition like "add econ0110" or "timer"
+    // value - Vertex that is the destination of the transition
 
-    public Vertex(TrafficState l) {
+    public Vertex(StateType l) {
         this.label = l;
         this.nextStates = new HashMap<>();
     }
 
-    public Vertex getNextState(String transition) {
+    public Vertex<StateType> getNextState(String transition) {
         return this.nextStates.get(transition);
     }
 
-    public boolean canVisitBad(Function<TrafficState, Boolean> badProperty) {
+    public boolean canVisitBad(Function<StateType, Boolean> badProperty) {
         return this.canVisitBad_helper(badProperty, new HashSet<>());
     }
 
     private boolean canVisitBad_helper(
-            Function<TrafficState, Boolean> badProperty, HashSet<Vertex> visited) {
-        // Will fill in during lecture
+            Function<StateType, Boolean> badProperty, HashSet<Vertex<StateType>> visited) {
+        // Will fill in during next lecture
         return false;
     }
 
@@ -32,8 +35,8 @@ public class Vertex {
         return this.canLoop_helper(new HashSet<>());
     }
 
-    private boolean canLoop_helper(HashSet<TrafficState> visited) {
-        // Will fill in during lecture
+    private boolean canLoop_helper(HashSet<StateType> visited) {
+        // Will fill in during next lecture
         return false;
     }
 }
